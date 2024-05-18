6b9bd4981f7e1ce7f1383b34e6e64f5eff834274
martinrdrz <martinrdrz@hotmail.com>
2024-05-18 11:43:06 -0300

    se agregó de la seccion9: Profuncdizando Hooks

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,19 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react"
 
-export const useCouter = (initialValue) => {
-    const [counter, setCounter] = useState(initialValue);
 
-    const increment = (value = 1) => {
-        setCounter(counter + value);
-    };
+export const useCounter = ( initialValue = 10 ) => {
 
-    const decrement = () => {
-        setCounter(counter - 1);
-    };
+    const [ counter, setCounter ] = useState( initialValue )
+
+    const increment = ( value = 1 ) => {
+        setCounter( counter + value );
+    }
+
+    const decrement = ( value = 1 ) => {
+        // if ( counter === 0 ) return;
+
+        setCounter( counter - value );
+    }
 
     const reset = () => {
-        setCounter(initialValue);
-    };
+        setCounter( initialValue );
+    }
 
-    return { counter, increment, decrement, reset };
-};
+    return {
+        counter,
+        increment,
+        decrement,
+        reset,
+    }
+
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { HooksApp } from './HooksApp';
-import { CounterWithCustomHook } from './CounterWithCustomHook';
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
