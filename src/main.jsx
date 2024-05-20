2cfa576b2094e33ad9ba45a668308c28139c380c
martinrdrz <martinrdrz@hotmail.com>
2024-05-20 18:20:11 -0300

    se llamo a otro de los componenetes en el main

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import ReactDOM from 'react-dom/client';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 import './index.css';
-import { FocusScreen } from './04-useRef/FocusScreen';
+//import { FocusScreen } from './04-useRef/FocusScreen';
+//import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FocusScreen />
+        <CallbackHook />
     </React.StrictMode>
 );
