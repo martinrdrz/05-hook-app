65221380c872ce5d81c799e0a066c5d96891ea22
martinrdrz <martinrdrz@hotmail.com>
2024-05-20 14:10:36 -0300

    seactualizo uno de los ejemplos

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { HooksApp } from './HooksApp';
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 import './index.css';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterWithCustomHook />
+        <FocusScreen />
     </React.StrictMode>
 );
