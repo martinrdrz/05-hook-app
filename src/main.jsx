f04b551006d4acf5c0eb7541c917933b628e9f64
martinrdrz <martinrdrz@hotmail.com>
2024-05-21 13:37:35 -0300

    aplicacion completa de todos con useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom/client';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import './index.css';
 //import { FocusScreen } from './04-useRef/FocusScreen';
 //import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+//import { CallbackHook } from './06-memos/CallbackHook';
+//import {Padre} from './07-tarea-memo/Padre'
+import './index.css';
+import { TodoApp } from './08-useReducer/TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CallbackHook />
+        <TodoApp />
     </React.StrictMode>
 );
